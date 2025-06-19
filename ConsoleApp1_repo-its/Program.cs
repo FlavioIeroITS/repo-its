ed095bcac69624885c2717f658e63aec5ede0b43
FlavioIeroITS <flavio.iero.14-2@accademiadigitaleliguria.it>
2025-06-19 11:38:13 +0200

    prova c#

---
+++ ConsoleApp1_repo-its/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1_repo_its
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
