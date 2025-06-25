3f49d41fb6775a7af3e44c398c5f510c7d5aa51b
FlavioIeroITS <flavio.iero.14-2@accademiadigitaleliguria.it>
2025-06-25 15:32:30 +0200

    #1 update Calcolatrice

---
+++ CalcolatriceLibreria/Calcolatrice.cs
@@ -0,0 +1,30 @@
+﻿namespace CalcolatriceLibreria
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice() { }
+
+        public double Somma(double x, double y)
+        {
+
+            return x + y;
+            throw new NotImplementedException();
+        }
+
+        public double Sottrazione(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Moltiplicazione(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divisione(double x, double y)
+        {
+            if(y != 0) return x / y;
+            return 0;
+        }
+    }
+}
---
+++ TestCalcolatrice/Test1.cs
@@ -0,0 +1,38 @@
+﻿using CalcolatriceLibreria;
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5");
+        }
+
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 5");
+        }
+
+        [TestMethod]
+        public void TestSommaTanteSomme()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                // creo due numeri casuali ed eseguo il test
+                Random random = new Random();
+                double x = random.NextDouble() * 100; // Numero casuale tra 0 e 100
+                double y = random.NextDouble() * 100; // Numero casuale tra 0 e 100
+                Calcolatrice calcolatrice = new Calcolatrice();
+                double risultato = calcolatrice.Somma(x, y);
+                Assert.AreEqual(x + y, risultato, $"La somma di {x} e {y} dovrebbe essere {x + y}");
+            }
+        }
+    }
+}
